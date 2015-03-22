dd297dae729ee0cb7ff7bda1f3a4655583c65af2
mmapzlh9 <liam.higgins@student.manchester.ac.uk>
2015-03-22 07:26:30 +0000

    facebook button

---
+++ AR/app/src/main/java/uk/co/barnaby_taylor/ar/MainFragment.java
@@ -0,0 +1,22 @@
+package uk.co.barnaby_taylor.ar;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by liam on 22/03/15.
+ */
+public class MainFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.main, container, false);
+
+        return view;
+    }
+
+}
